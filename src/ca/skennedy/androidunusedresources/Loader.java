8dd87b84f4d23f33a75fb2d7be8df9549cfcd564
Scott Kennedy <skennedy27@gmail.com>
2010-11-05 15:35:04 +0000

    Removed some testing code

--- src/ca/skennedy/androidunusedresources/Loader.java
+++ src/ca/skennedy/androidunusedresources/Loader.java
@@ -2,7 +2,6 @@ package ca.skennedy.androidunusedresources;
 
 public class Loader {
     public static void main(final String[] args) {
-        //final ResourceScanner resourceScanner = new ResourceScanner("C:\\Users\\Scott\\Documents\\Homick\\Cineplex Mobile\\android\\trunk");
         final ResourceScanner resourceScanner = new ResourceScanner();
         resourceScanner.run();
     }
