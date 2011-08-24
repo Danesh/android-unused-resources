7637d401e534b41dd08e2fca4367dc05e6785975
Scott Kennedy <skennedy27@gmail.com>
2011-08-24 01:49:00 +0000

    Find declarations of color resources

--- src/ca/skennedy/androidunusedresources/ResourceScanner.java
+++ src/ca/skennedy/androidunusedresources/ResourceScanner.java
@@ -43,11 +43,38 @@ public class ResourceScanner {
         // anim
         // array (referenced by string-array)
         // bool
-        // color
         // dimen
         // menu
         // plurals
         // xml
+        
+        // color
+        sResourceTypes.put("color", new ResourceType("color") {
+            @Override
+            public boolean doesFileDeclareResource(final File parent, final String fileName,
+                    final String fileContents, final String resourceName) {
+                // Check if we're in a valid directory
+                if (!parent.isDirectory()) {
+                    return false;
+                }
+                
+                final String directoryType = parent.getName().split("-")[0];
+                if (!directoryType.equals("values")) {
+                    return false;
+                }
+                
+                // Check if the resource is declared here
+                final Pattern pattern = Pattern.compile("<color.*?name\\s*=\\s*\"" + resourceName + "\".*?/?>");
+                
+                final Matcher matcher = pattern.matcher(fileContents);
+                
+                if (matcher.find()) {
+                    return true;
+                }
+                
+                return false;
+            }
+        });
         
         // drawable
         sResourceTypes.put("drawable", new ResourceType("drawable") {
