78e46fde123b69a895e8c8e7ca811f0262a43188
Scott Kennedy <skennedy27@gmail.com>
2012-01-08 03:48:43 +0000

    Find location of unused dimen resources

--- src/ca/skennedy/androidunusedresources/ResourceScanner.java
+++ src/ca/skennedy/androidunusedresources/ResourceScanner.java
@@ -44,7 +44,6 @@ public class ResourceScanner {
     static {
         // TODO: find declarations of these resources
         // bool
-        // dimen
         // styleable
 
         // anim
@@ -174,6 +173,33 @@ public class ResourceScanner {
 
                 // Check if the resource is declared here
                 final Pattern pattern = Pattern.compile("<color.*?name\\s*=\\s*\"" + resourceName + "\".*?/?>");
+
+                final Matcher matcher = pattern.matcher(fileContents);
+
+                if (matcher.find()) {
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        // dimen
+        sResourceTypes.put("dimen", new ResourceType("dimen") {
+            @Override
+            public boolean doesFileDeclareResource(final File parent, final String fileName, final String fileContents, final String resourceName) {
+                // Check if we're in a valid directory
+                if (!parent.isDirectory()) {
+                    return false;
+                }
+
+                final String directoryType = parent.getName().split("-")[0];
+                if (!directoryType.equals("values")) {
+                    return false;
+                }
+
+                // Check if the resource is declared here
+                final Pattern pattern = Pattern.compile("<dimen.*?name\\s*=\\s*\"" + resourceName + "\".*?/?>");
 
                 final Matcher matcher = pattern.matcher(fileContents);
 
